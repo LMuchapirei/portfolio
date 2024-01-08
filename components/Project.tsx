b958619ae8521d773186047335d4515ccea28151
lmuchapirei <muchapireil@gmail.com>
2024-01-08 15:08:07 +0200

    Adjust even styles changed due to wrapper div added to section as parent

--- components/Project.tsx
+++ components/Project.tsx
@@ -26,12 +26,12 @@ type ProjectProps = typeof projectsData[number]
         scale:scaleProgress,
         opacity:opacityProgress
     }}
-    className="mb-3 sm:mb-8 last:mb-0"
+    className="group mb-3 sm:mb-8 last:mb-0 "
     >
     <section
-    className="group bg-gray-100 max-w-[42rem] border 
-    border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  even:pl-8 hover:bg-gray-200 transition">
-    <div className='pt-4 pb-6 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] '>
+    className="bg-gray-100 max-w-[42rem] border 
+    border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  even:pl-8 hover:bg-gray-200 transition rounded-lg">
+    <div className='pt-4 pb-6 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] group-even:pl-8'>
     <h3 className="text-2xl font-semibold">{title}</h3>
       <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
       <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>

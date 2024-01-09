c3872dfedb3d815aa1aa612ed75495b5f6df3d88
lmuchapirei <muchapireil@gmail.com>
2024-01-09 18:46:14 +0200

    move to section change nav item on scroll

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import SectionHeading from './SectionHeading'
 import { projectsData } from '@/lib/data'
 import Project from './Project'
+import { useInView } from 'react-intersection-observer'
+import { useActiveSectionContext } from '@/context/activeSectionContext'
 export default function Projects() {
+  const {ref,inView} = useInView({threshold:0.5})
+  const {setActiveSection} = useActiveSectionContext()
+  useEffect(()=>{
+    if(inView){
+      setActiveSection("Projects")
+    }
+  },[inView,setActiveSection])
   return (
-    <section id="projects" className='scroll-mt-28'>
+    <section id="projects" className='scroll-mt-28' ref={ref}>
         <SectionHeading title='My Projects' children/>
         <div>
           { projectsData.map((project,index)=>(

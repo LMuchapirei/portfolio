5f0b3cf3775b05b6a2f7905ef3b2c822a448185c
lmuchapirei <muchapireil@gmail.com>
2024-01-09 18:44:26 +0200

    disable observer scroll when user clicks

--- context/activeSectionContext.tsx
+++ context/activeSectionContext.tsx
@@ -9,15 +9,18 @@ type ActiveSectionContextProviderProps = {
 }
 type ActiveSectionContextType = {
     activeSection: SectionName
-    setActiveSection:React.Dispatch<React.SetStateAction<SectionName>>
+    setActiveSection:React.Dispatch<React.SetStateAction<SectionName>>,
+    timeOfLastClick:number,
+    setTimeOfLastClick:React.Dispatch<React.SetStateAction<number>>,
 }
 
 export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null)
 export default function ActiveSectionContextProvider({children}:ActiveSectionContextProviderProps) {
   const [activeSection,setActiveSection] = useState<SectionName>('Home')
+  const [timeOfLastClick,setTimeOfLastClick] = useState(0) // Disable the observer api for a bit when user clicks
   return (
-    <ActiveSectionContext.Provider value={{activeSection,setActiveSection}}>
-        children
+    <ActiveSectionContext.Provider value={{activeSection,setActiveSection,timeOfLastClick,setTimeOfLastClick}}>
+       {children}
     </ActiveSectionContext.Provider>
   )
 }

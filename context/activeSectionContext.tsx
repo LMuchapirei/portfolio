9249cd5934caa6876193d9da216c51d24b594651
lmuchapirei <muchapireil@gmail.com>
2024-01-09 19:10:53 +0200

    refactor types to own file

--- context/activeSectionContext.tsx
+++ context/activeSectionContext.tsx
@@ -1,20 +1,10 @@
 "use client"
+import { ActiveSectionContextProviderProps, ActiveSectionContextType, SectionName } from '@/lib/types'
 import React, { useState,createContext, useContext } from 'react'
-import { links } from "@/lib/data"
 
-
-type SectionName = typeof links[number]["name"]
-type ActiveSectionContextProviderProps = {
-    children:React.ReactNode
-}
-type ActiveSectionContextType = {
-    activeSection: SectionName
-    setActiveSection:React.Dispatch<React.SetStateAction<SectionName>>,
-    timeOfLastClick:number,
-    setTimeOfLastClick:React.Dispatch<React.SetStateAction<number>>,
-}
 
 export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null)
+
 export default function ActiveSectionContextProvider({children}:ActiveSectionContextProviderProps) {
   const [activeSection,setActiveSection] = useState<SectionName>('Home')
   const [timeOfLastClick,setTimeOfLastClick] = useState(0) // Disable the observer api for a bit when user clicks
@@ -25,12 +15,10 @@ export default function ActiveSectionContextProvider({children}:ActiveSectionCon
   )
 }
 
-
 export function useActiveSectionContext(){
     const context = useContext(ActiveSectionContext)
     if(context === null){
         throw new Error("useActiveSectionContext must be used within a ActiveSectionContextProvider")
     }
-
     return context;
 }
